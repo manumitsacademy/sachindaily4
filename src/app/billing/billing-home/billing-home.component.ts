b18eab1d888917d19ad245766c0dcaf97df57abf
manumitsacademy <manumits.info@gmail.com>
2019-04-08 10:18:19 +0530

    'billingDisplay

--- src/app/billing/billing-home/billing-home.component.ts
+++ src/app/billing/billing-home/billing-home.component.ts
@@ -10,15 +10,23 @@ import { _ } from 'underscore';
 export class BillingHomeComponent implements OnInit {
 
   constructor(public pS:ProductService,public dS:DeliveryService) { }
-
+  mobileNumbers=[];
+  products=[];
+  productIds=[];
+  deliveredProductsData={};
   ngOnInit() {
     this.dS.getAllDeliveries().subscribe(res=>{
       console.log("All deliveries::",res);
       var x = _.groupBy(res,'mobileNumber');
       for(let key in x){
+        this.mobileNumbers.push({'mobileNumber':key,'flatNumber':x[key][0].flatNumber})
         var y = _.groupBy(x[key],'productId')
         console.log("y:::",y);
+        this.deliveredProductsData[key]=y;
         for(let k in y){
+          if(this.products.indexOf(k)==-1){
+            this.products.push({'productId':k,'productName':y[k][0].productName});
+          }
           var z = y[k].reduce((a,b)=>{
             if(a.cost){
               return (a.cost*a.quantity)+(b.cost*b.quantity);
@@ -27,11 +35,14 @@ export class BillingHomeComponent implements OnInit {
               return a+(b.cost*b.quantity);
             }
           })
-          console.log(z);
         }
-        x[key]=y;
       }
       console.log(x);
+      console.log(this.deliveredProductsData);
+      console.log(this.mobileNumbers);
+      this.products=_.indexBy(this.products,'productId');
+      this.productIds = _.allKeys(this.products);
+      console.log(this.productIds)
     })
   }
 
