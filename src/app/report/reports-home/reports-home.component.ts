0b317f004c3e89f4c5b1b34fc3257c9a3599b627
Spam7 <shubhammunjal74@gmail.com>
2019-05-07 11:42:49 +0530

    shibh

--- src/app/report/reports-home/reports-home.component.ts
+++ src/app/report/reports-home/reports-home.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateStruct, NgbCalendar, NgbDate} from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
-import { CustomerService } from 'src/app/customer/customer.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-reports-home',
   templateUrl: './reports-home.component.html',
-  styleUrls: ['./reports-home.component.css']
+  styleUrls: ['./reports-home.component.css'],
+  providers: [DatePipe]
 })
 export class ReportsHomeComponent implements OnInit {
   
   model: NgbDateStruct;
-  date: {year: number, month: number,day: number};
+  date: {year: number, month: number};
+  i;
+  
+  myDate = new Date();
+  currdate = this.datePipe.transform(this.myDate, 'dd');
+  currmonth = this.datePipe.transform(this.myDate, 'M');
+  curryear =  this.datePipe.transform(this.myDate, 'yyyy');
+  newdate: NgbDateStruct = { year: parseInt(this.curryear), month:  parseInt(this.currmonth), day:  parseInt(this.currdate) };
+  // { year: 2020, month: 7, day: 14 }; // July, 14 1789
 
-  constructor(private calendar: NgbCalendar,public router:Router,public cS:CustomerService) { }
+  constructor(private calendar: NgbCalendar,public router:Router,private datePipe: DatePipe) { 
+   
+  }
+
+  isDisabled = (date: NgbDate, current: {date: number,month: number}) => date.after(this.newdate) ;
 
   ngOnInit() {
+    // for(this.i=this.currdate;this.i>=this.currdate;this.i++){
+      console.log(this.currdate);
+    //   // const isDisabled = (date: NgbDate, current: {month: number}) => date.day === 13;
+    // }
   }
+
+
   generateReport(sDate){
+    console.clear();
     console.log("Log cleared at generate Report")
     console.log(sDate);
     this.router.navigate(['/reports/wingWiseReport'],{queryParams: sDate })
   }
 }
+ 
